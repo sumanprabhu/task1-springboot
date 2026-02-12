2b1bb8f5fa6ac8e174812eaed6854445d631b4d9
Suman Prabhu <sumanprabhu268@gmail.com>
2026-02-12 12:22:59 +0530

    modified: create a constructor for role repo

--- src/main/java/com/task1/suman/service/UserService.java
+++ src/main/java/com/task1/suman/service/UserService.java
@@ -1,9 +1,13 @@
 package com.task1.suman.service;
 
+import com.task1.suman.model.Address;
+import com.task1.suman.model.Role;
 import com.task1.suman.model.User;
+import com.task1.suman.repo.RoleRepo;
 import com.task1.suman.repo.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -15,6 +19,9 @@ public class UserService {
 
     @Autowired
     private UserRepo userRepo;
+
+    @Autowired
+    private RoleRepo roleRepo;
 
     public List<User> getUsers() {
         return userRepo.findAll();
@@ -34,11 +41,43 @@ public class UserService {
     public User updateUser(UUID id,User user) {
         User existingUser = userRepo.findById(id)
                 .orElseThrow(
-                    ()->new ResponseStatusException(HttpStatus.NOT_FOUND,"user not found with the id :")
+                    ()->new ResponseStatusException(HttpStatus.NOT_FOUND,"user not found with the id :" +id)
                 );
         existingUser.setName(user.getName());
         existingUser.setEmail(user.getEmail());
         existingUser.setContactNum(user.getContactNum());
+
+        if(user.getAddress()!=null){
+            Address existingAddress = existingUser.getAddress();
+            if(existingAddress==null)
+                existingAddress=new Address();
+            existingAddress.setStreet(user.getAddress().getStreet());
+            existingAddress.setCity(user.getAddress().getCity());
+            existingAddress.setState(user.getAddress().getState());
+            existingAddress.setZipCode(user.getAddress().getZipCode());
+
+            existingUser.setAddress(existingAddress);
+        }
+
+        if (user.getRoles() != null && !user.getRoles().isEmpty()) {
+
+            List<Long> roleIds = user.getRoles()
+                    .stream()
+                    .map(Role::getId)
+                    .toList();
+
+            List<Role> rolesFromDb = roleRepo.findAllById(roleIds);
+
+            if (rolesFromDb.size() != roleIds.size()) {
+                throw new ResponseStatusException(
+                        HttpStatus.BAD_REQUEST,
+                        "One or more roles are invalid"
+                );
+            }
+
+            existingUser.setRoles(rolesFromDb);
+        }
+
         return userRepo.save(existingUser);
     }
 
@@ -49,4 +88,47 @@ public class UserService {
     public List<User> getUserByName(String name) {
         return userRepo.findAllByNameContainingIgnoreCase(name);
     }
+
+    public User assignRole(UUID id, Long roleID) {
+        User user = userRepo.findById(id)
+                .orElseThrow(() ->
+                        new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found")
+                );
+        Role role = roleRepo.findById(roleID)
+                .orElseThrow(()->
+                        new ResponseStatusException(HttpStatus.NOT_FOUND,"Role not found")
+                );
+        if(user.getRoles().contains(role))
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"User already has this role");
+        user.getRoles().add(role);
+        return userRepo.save(user);
+    }
+
+    public User removeRole(UUID id, Long roleId) {
+        User user = userRepo.findById(id)
+                .orElseThrow(() ->
+                        new ResponseStatusException(
+                                HttpStatus.NOT_FOUND,
+                                "User not found"
+                        )
+                );
+
+        Role role = roleRepo.findById(roleId)
+                .orElseThrow(() ->
+                        new ResponseStatusException(
+                                HttpStatus.NOT_FOUND,
+                                "Role not found"
+                        )
+                );
+
+        if (!user.getRoles().contains(role)) {
+            throw new ResponseStatusException(
+                    HttpStatus.BAD_REQUEST,
+                    "User does not have this role"
+            );
+        }
+        user.getRoles().remove(role);
+        return userRepo.save(user);
+    }
+
 }
