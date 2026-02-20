3ab2f20438e2249c14d8d2bcea25338f9ca9ac09
Suman Prabhu <sumanprabhu268@gmail.com>
2026-02-20 17:06:40 +0530

    modified: removed roleName

--- backend/src/main/java/com/task1/suman/dto/RegisterRequest.java
+++ backend/src/main/java/com/task1/suman/dto/RegisterRequest.java
@@ -13,5 +13,4 @@ public class RegisterRequest {
     private String email;
     private String password;
     private String contactNum;
-    private String roleName;  // Just send "ADMIN" or "USER" as string instead of sending entire Role object!
 }
